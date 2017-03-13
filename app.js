2a1e5b8eafcdd154ab81f54e95c9752342ef8b3f
chq-marioj <mario.o.jimenez@expeditors.com>
2017-03-13 11:00:29 -0700

    style changes and new html elements

--- app.js
+++ app.js
@@ -1,3 +1,11 @@
 var budgetController = (function BudgetController() {
-    
+
+});
+var UIController = (function UIController() {
+
+});
+var AppController = (function AppController(BudgetController, UIController) {
+    document.querySelector('.add-btn').addEventListener('click', function () {
+        console.log('Cliked!');
+    });
 });
