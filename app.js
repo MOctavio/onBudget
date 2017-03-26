a4708764a3d7956f499a7ba62b1c5a1b9eafe391
Mario Octavio Jimenez <jimenez303@gmail.com>
2017-03-26 15:40:29 -0700

    appcontroller init an uicontroller

--- app.js
+++ app.js
@@ -1,14 +1,48 @@
-(function() {
-    console.info('App initialized');
+var budgetController = (function BudgetController() {})();
 
-    var budgetController = (function BudgetController() {});
-    var UIController = (function UIController() {});
-    var AppController = (function AppController(BudgetController, UIController) {
-        document.querySelector('.btn-add').addEventListener('click', function(event) {
-            event.preventDefault();
-            console.log('Cliked!');
-        });
-    });
+var uIController = (function UIController() {
+    var DOMSelector = {
+        inputType: '.js-add-type',
+        inputDescription: '.js-add-description',
+        inputValue: '.js-add-value',
+        btnAdd: '.js-add-btn'
+    };
 
-    AppController();
+    function _getDOMSelector() {
+        return DOMSelector;
+    }
+    function _getInput() {
+        return {
+            type: document.querySelector(DOMSelector.inputType).value,
+            description: document.querySelector(DOMSelector.inputDescription).value,
+            value: document.querySelector(DOMSelector.inputValue).value
+        };
+    }
+    return {getInput: _getInput, getDOMSelector: _getDOMSelector};
 })();
+
+var AppController = (function AppController(budgetController, uIController) {
+    function addItem(event) {
+      event.preventDefault();
+      console.log('Event to add an has been entry registered.');
+    }
+
+    function setupEventListeners() {
+        var DOMSelector = uIController.getDOMSelector();
+
+        document.querySelector(DOMSelector.btnAdd).addEventListener('click', addItem);
+        document.querySelector(DOMSelector.inputValue).addEventListener('keypress', function(e) {
+            if (e.keyCode == 13) {
+                addItem(e);
+            }
+            return;
+        });
+    }
+
+    function _init() {
+      setupEventListeners();
+    }
+    return {init: _init};
+})(budgetController, uIController);
+
+AppController.init();
